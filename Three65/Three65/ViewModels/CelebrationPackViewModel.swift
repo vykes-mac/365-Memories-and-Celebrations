93dc5dc700478468ed8b30824361ee1a1bf4ac65
vykes <knrymcleish@gmail.com>
2026-02-01 15:32:06 -0500

    fix missing imports

--- Three65/Three65/ViewModels/CelebrationPackViewModel.swift
+++ Three65/Three65/ViewModels/CelebrationPackViewModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftData
+import Combine
 
 @MainActor
 final class CelebrationPackViewModel: BaseViewModel {
--- Three65/Three65/Views/CelebrationPack/CollageFlowView.swift
+++ Three65/Three65/Views/CelebrationPack/CollageFlowView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct CollageFlowView: View {
     @Environment(\.dismiss) private var dismiss
